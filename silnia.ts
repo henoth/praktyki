620d79b888e0ff77a82027b5d0b3bc54a507b1a4
henoth <henocik997@gmail.com>
2021-05-19 16:21:28 +0200

    silnia na 2 sposoby

--- silnia.ts
+++ silnia.ts
@@ -1,13 +1,20 @@
-let liczba: number = 20;
-let silnia: number = 1;
-for (let i = 1; i <= liczba; i++) {
-    if (liczba > 0) {
-        silnia = silnia * i;
-    }
-    else {
-        console.log("silnia równa się 1")
-        break;
-    }
+// let liczba: number = 20;
+// let silnia: number = 1;
+// for (let i = 1; i <= liczba; i++) {
+//     if (liczba > 0) {
+//         silnia = silnia * i;
+//     }
+//     else {
+//         console.log("silnia równa się 1")
+//         break;
+//     }
+// }
+// console.log("silnia równa się " + silnia)
 
+function silnia(n = 5)
+{
+    if(n==0)
+    return 1;
+    return silnia(n -1) * n;
 }
-console.log("silnia równa się " + silnia)
+console.log(silnia());
